4c73d9be294d41b516cf4828fb91d8501a4163e4
kelsey makale <sianwamak@gmail.com>
2021-03-31 12:37:03 +0300

    Completed new layout

--- app/src/main/java/com/example/githubissuetracker/activities/SingleIssueActivity.java
+++ app/src/main/java/com/example/githubissuetracker/activities/SingleIssueActivity.java
@@ -1,10 +1,13 @@
 package com.example.githubissuetracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.githubissuetracker.R;
+import com.example.githubissuetracker.adapters.SingleIssueCommentsAdapter;
 
 public class SingleIssueActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class SingleIssueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_issue);
+
+        //componets
+        RecyclerView commentsRecyclerview = findViewById(R.id.issues_comments);
+        commentsRecyclerview.setHasFixedSize(true);
+        commentsRecyclerview.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+        commentsRecyclerview.setAdapter(new SingleIssueCommentsAdapter());
     }
 }
